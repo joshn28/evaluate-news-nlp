88b9a26a922c690c8a556a907a674e33d5cf0005
Joshua Nobleza <joshnobleza@bell.net>
2023-05-18 18:14:52 -0400

    feat: Add GET route for API key

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 
 const app = express();
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'));
 
@@ -12,5 +23,9 @@ const port = 3000;
 
 const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
+});
+
+app.get('/key', (req, res) => {
+    res.send({ apiKey: process.env.API_KEY });
 });
 
