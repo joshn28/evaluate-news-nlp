1a3d55249e229cb3686e0b80a7037aa2e68aa4e5
Joshua Nobleza <joshnobleza@bell.net>
2023-05-19 16:11:57 -0400

    feat: Add functionality to update UI with data from API

---
+++ src/client/index.js
@@ -0,0 +1,20 @@
+import { getAnalysis } from './js/getAnalysis'
+import { getKey } from './js/getKey'
+import { updateUI } from './js/updateUI'
+
+import '../client/styles/base.sass'
+import '../client/styles/form.sass'
+import '../client/styles/results.sass'
+import '../client/styles/footer.sass'
+
+document.getElementById('submit').addEventListener('click', evt => {
+    evt.preventDefault();
+
+    const content = evt.target.previousElementSibling.value;
+
+    getKey()
+    .then(key => getAnalysis(key, content))
+    .then(function(data) {
+        updateUI(data);
+    });
+});
