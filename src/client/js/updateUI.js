be6f24f0bb4d6b30d0d80af7a0ddf72a85805cc2
Joshua Nobleza <joshnobleza@bell.net>
2023-05-24 11:00:48 -0400

    feat: Add functionality to update the results dynamically

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,8 +1,8 @@
 import checkMark from '../assets/checkmark.svg';
 import cross from '../assets/red-x.svg';
 
-// Return the correct img object based on the result string
-const getImg = result => {
+// Return the correct icon based on the result string
+const getIcon = result => {
     const checkMarkValues = ['agreement', 'subjective', 'ironic'];
     const img = new Image();
 
@@ -15,27 +15,54 @@ const getImg = result => {
     return img;
 };
 
+// Render the text and the icon
+function updateResult(elementId, value, text) {
+    const element = document.getElementById(elementId);
+    element.firstElementChild.textContent = text;
+
+    const icon = getIcon(value);
+    icon.classList.add('hidden');
+
+    // Remove previous icon rendered previously
+    if (element.lastChild.tagName === 'IMG') {
+        element.lastChild.remove();
+    }
+
+    element.appendChild(icon);
+  }
+
 // Update the UI based on the values from the received data
 const updateUI = (data) => {
     const { score_tag, agreement, subjectivity, confidence, irony } = data;
 
+    const results = document.querySelector('section');
+    results.classList.add('hidden');
+    results.classList.remove('hidden');
+    results.classList.add('animate__animated', 'animate__fadeInDown');
+
     document.getElementById('score').textContent = `Score: ${score_tag}`;
 
-    const agreementRes = document.getElementById('agreement');
-    agreementRes.firstElementChild.textContent = 'Agreement';
-    agreementRes.appendChild(getImg(agreement));
-
-    const subjectivityRes = document.getElementById('subjectivity');
-    subjectivityRes.firstElementChild.textContent = 'Subjective';
-    subjectivityRes.appendChild(getImg(subjectivity));
-
-    const ironyRes = document.getElementById('irony');
-    ironyRes.firstElementChild.textContent = 'Ironic';
-    ironyRes.appendChild(getImg(irony));
+    updateResult('agreement', agreement, 'Agreement');
+    updateResult('subjectivity', subjectivity, 'Subjective');
+    updateResult('irony', irony, 'Ironic');
 
     const confidenceRes = document.getElementById('confidence');
     confidenceRes.setAttribute('value', confidence);
     confidenceRes.previousElementSibling.textContent = `Confidence: ${confidence}`;
+
+    results.addEventListener('animationend', () => {
+        const values = ['agreement', 'subjectivity', 'irony'];
+
+        for (const val of values) {
+            const element = document.getElementById(val);
+            const icon = element.lastElementChild;
+
+            icon.classList.remove('hidden');
+            icon.classList.add('animate__animated', 'animate__bounceIn');
+        }
+
+        results.classList.remove('animate__animated', 'animate__fadeInDown');
+    });
 };
 
 export {
