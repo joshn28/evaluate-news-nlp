7eec7103d00bc438787ec74ee03de22b3cf44193
Joshua Nobleza <joshnobleza@bell.net>
2023-05-26 15:56:57 -0400

    test: Update test for getKey function

--- __tests__/getKey.spec.js
+++ __tests__/getKey.spec.js
@@ -1,15 +1,20 @@
 import { getKey }  from '../src/client/js/getKey';
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+global.fetch = jest.fn().mockResolvedValue({
+    // Mock the response
+    json: jest.fn().mockResolvedValue({ apiKey: process.env.API_KEY }),
+});
+
 describe('Testing the getKey function', () => {
     test('it should make a get request to /key route', async () => {
-        global.fetch = jest.fn().mockResolvedValue({
-            // Mock the response
-            json: jest.fn().mockResolvedValue({ data: 'test' }),
-        });
     
-        await getKey();
+        const result = await getKey();
     
         expect(global.fetch).toHaveBeenCalledTimes(1);
         expect(global.fetch).toHaveBeenCalledWith('/key');
+        expect(result).toEqual(process.env.API_KEY);
     });
 });
